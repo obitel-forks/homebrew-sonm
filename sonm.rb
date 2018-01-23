7ce0d1d6f047732d5e1ff4677a914647200f80bf
Roman Sivakov <romansivakov@gmail.com>
2018-01-23 11:38:27 +0300

    Update sonm.rb

--- sonm.rb
+++ sonm.rb
@@ -5,8 +5,10 @@ class Sonm < Formula
     sha256 "cfb6a36ae8dab5a84be5c0f28247bc2ce973546752f3d136d5a813cc81b536e0"
    depends_on "docker"
    def install
- 
-   #system "./configure", "--prefix=#{"/usr/local/Cellar/sonm/64"}"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "unzip ~/Library/Caches/Homebrew/sonm-64.zip /usr/local/Cellar/sonm"
    #system "cd /usr/local/Cellar/sonm/64", "unzip ./sonm_v0.3.0_darwin64.zip ./sonm"
    #system "ln ./sonm/cli/sonmcli_darwin64 "
@@ -29,7 +31,6 @@ bottle do
   root_url "https://sonm.com"   
   prefix "/sonm/64"   
   cellar "/opt/homebrew/Cellar"   
-  # revision 4
 end
 
 end
